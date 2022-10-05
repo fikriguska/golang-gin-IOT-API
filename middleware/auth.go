c3b79d6716a2028a10c964e8fc7cee9ad9ffec92
Bintang Fikriguska <bintangf00code@gmail.com>
2022-10-05 14:54:44 +0700

    refactor: use native for getenv

--- middleware/auth.go
+++ middleware/auth.go
@@ -1,63 +1,16 @@
 package middleware
 
 import (
-	"encoding/base64"
 	"log"
 	"net/http"
 	e "src/error"
 	"src/models"
 	"src/service/user_service"
-	"strings"
 	"time"
 
 	jwt "github.com/appleboy/gin-jwt/v2"
 	"github.com/gin-gonic/gin"
 )
-
-func BasicAuth() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		auth := strings.SplitN(c.Request.Header.Get("Authorization"), " ", 2)
-
-		if len(auth) != 2 || auth[0] != "Basic" {
-			c.JSON(http.StatusUnauthorized, gin.H{
-				"status": "error",
-				"data":   "invalid authorization key",
-			})
-			c.Abort()
-			return
-		}
-		log.Println(auth)
-
-		payload, _ := base64.StdEncoding.DecodeString(auth[1])
-
-		pair := strings.SplitN(string(payload), ":", 2)
-
-		userService := user_service.User{
-			User: models.User{
-				Username: pair[0],
-				Password: pair[1],
-			},
-		}
-		credCorrect, activated := userService.Auth()
-		if len(pair) != 2 || !(credCorrect && activated) {
-
-			c.JSON(http.StatusUnauthorized, gin.H{
-				"status": "error",
-				"data":   "invalid authorization key",
-			})
-			c.Abort()
-			return
-		}
-		id, _, pass, isAdmin := userService.Get()
-
-		c.Set("id_user", id)
-		c.Set("username", pair[0])
-		c.Set("password", pass)
-		c.Set("is_admin", isAdmin)
-
-		c.Next()
-	}
-}
 
 var JwtMiddleware, _ = jwt.New(&jwt.GinJWTMiddleware{
 	Realm:       "test zone",
