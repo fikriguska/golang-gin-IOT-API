4183272856e1dca5b92bf9e4be0eeca8c4017f02
Bintang Fikriguska <bintangf00code@gmail.com>
2023-05-01 08:36:13 +0700

    refactor: simplify jwt conf

--- middleware/auth.go
+++ middleware/auth.go
@@ -12,11 +12,8 @@ import (
 )
 
 var JwtMiddleware, _ = jwt.New(&jwt.GinJWTMiddleware{
-	Realm:       "test zone",
-	Key:         []byte("s3cr3tz_k3y"),
-	Timeout:     time.Hour,
-	MaxRefresh:  time.Hour,
-	IdentityKey: "identity",
+	SigningAlgorithm: "HS256",
+	Key:              []byte("s3cr3tz_k3y"),
 	PayloadFunc: func(data interface{}) jwt.MapClaims {
 		if v, ok := data.(*models.User); ok {
 			return jwt.MapClaims{
@@ -26,36 +23,13 @@ var JwtMiddleware, _ = jwt.New(&jwt.GinJWTMiddleware{
 		}
 		return jwt.MapClaims{}
 	},
-	IdentityHandler: func(c *gin.Context) interface{} {
-		claims := jwt.ExtractClaims(c)
-		return &models.User{
-			Id:       int(claims["id"].(float64)),
-			Is_admin: claims["is_admin"].(bool),
-		}
-	},
+
 	Authenticator: func(c *gin.Context) (interface{}, error) {
-		// var loginVals login
-		// if err := c.ShouldBind(&loginVals); err != nil {
-		// 	return "", jwt.ErrMissingLoginValues
-		// }
-		// userID := loginVals.Username
-		// password := loginVals.Password
-
-		// if (userID == "admin" && password == "admin") || (userID == "test" && password == "test") {
-		// 	return &User{
-		// 		UserName:  userID,
-		// 		LastName:  "Bo-Yi",
-		// 		FirstName: "Wu",
-		// 	}, nil
-		// }
-
-		// return nil, jwt.ErrFailedAuthentication
 
 		var json models.UserLogin
 
 		// Check required parameter
 		if err := c.ShouldBindJSON(&json); err != nil {
-			// errorResponse(c, http.StatusBadRequest, e.ErrInvalidParams)
 			return nil, e.ErrInvalidParams
 		}
 
@@ -91,34 +65,4 @@ var JwtMiddleware, _ = jwt.New(&jwt.GinJWTMiddleware{
 			"expire": expire.Format(time.RFC3339),
 		})
 	},
-	// Authorizator: func(data interface{}, c *gin.Context) bool {
-	// 	if v, ok := data.(*models.User); ok && v.Username == "admin" {
-	// 		return true
-	// 	}
-
-	// 	return false
-	// },
-	// Unauthorized: func(c *gin.Context, code int, message string) {
-	// 	c.JSON(code, gin.H{
-	// 		"code":    code,
-	// 		"message": message,
-	// 	})
-	// },
-	// TokenLookup is a string in the form of "<source>:<name>" that is used
-	// to extract token from the request.
-	// Optional. Default value "header:Authorization".
-	// Possible values:
-	// - "header:<name>"
-	// - "query:<name>"
-	// - "cookie:<name>"
-	// - "param:<name>"
-	TokenLookup: "header: Authorization, query: token, cookie: jwt",
-	// TokenLookup: "query:token",
-	// TokenLookup: "cookie:token",
-
-	// TokenHeadName is a string in the header. Default value is "Bearer"
-	TokenHeadName: "Bearer",
-
-	// TimeFunc provides the current time. You can override it to use another time value. This is useful for testing or if your server uses a different time zone than your tokens.
-	TimeFunc: time.Now,
 })
